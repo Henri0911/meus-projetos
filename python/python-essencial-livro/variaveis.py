43b996573d37bfaa55a932c41a93e88897d206fe
Henrique <helio_h9@hotmail.com>
2025-10-01 15:49:56 -0300

    Add mais funções no python

--- python/python-essencial-livro/variaveis.py
+++ python/python-essencial-livro/variaveis.py
@@ -27,6 +27,8 @@ a = True
 b = False
 print(type(a)) #saída: <class 'bool'>
 
+print("***listas***")
+
 #lista de numeros inteiros
 numeros = [1, 2, 3, 4, 5]
 
@@ -35,6 +37,8 @@ frutas = ['maçã', 'banana', 'laranja']
 
 #lista com dados de diferentes tipos
 mista = [1, 'dois', 3.0, ['a', 'b', 'c']]
+
+print("***dicionarios***")
 
 #criando um dicionario com pares chave-valor
 pessoa = {'nome': 'João', 'idade': 30, 'cidade': 'São Paulo'}
@@ -54,5 +58,99 @@ print(pessoa)
 del pessoa['cidade']
 print(pessoa)
 #saída: {'nome': 'João', 'idade': 31, 'pais': 'Brasil'}
+
+print("***conjuntos***")
+
+#criacao de conjutos
+conjunto1 = {1, 2, 3, 4, 5}
+print(conjunto1) #saída: {1, 2, 3, 4, 5}
+
+#o que é uma lista set?
+#é uma coleção não ordenada de elementos únicos.
+#criacao de um conjunto a partir de uma lista set
+conjunto2 = set([1, 2, 3, 4, 5])
+print(conjunto2) #saída: {1, 2, 3, 4, 5}
+
+conjunto3 = {1, 2, 3, 4}
+conjunto4 = {3, 4, 5, 6}
+
+#união
+print(conjunto3 | conjunto4) #saída: {1, 2, 3, 4, 5, 6}
+#pega todos os elementos dos dois conjuntos, sem duplicatas
+
+#intersecao
+print(conjunto3 & conjunto4) #saída: {3, 4}
+#pega somente os elementos que estão em ambos os conjuntos
+
+#diferença
+print(conjunto3 - conjunto4) #saída: {1, 2}
+#pega os elementos que estão no conjunto3 mas não estão no conjunto4
+
+#diferença simétrica
+print(conjunto3 ^ conjunto4) #saída: {1, 2, 5, 6}
+#pega os elementos que estão em um dos conjuntos, mas não em ambos
+
+print("***tuplas***")
+
+#tuplas vazias
+tupla_vazia = ()
+print(tupla_vazia) #saída: ()
+
+#tupla com elementos
+tupla = 1, 2, 3, 4
+print(tupla) #saída: (1, 2, 3, 4)
+
+#tupla com elementos delimitados por parênteses
+tupla2 = (1, 2, 3, 4)
+print(tupla2) #saída: (1, 2, 3, 4)
+
+#acessando elementos da tupla
+tupla3 = (1, 2, 3, 4)
+print(tupla3[0]) #saída: 1
+print(tupla3[-1]) #saída: 4
+
+#tuplas com elementos de diferentes tipos
+tupla4 = (1, 2, 'três', 4.0, True)
+print(tupla4) #saída: (1, 2, 'três', 4.0, True)
+#tuplas são imutáveis, ou seja, não podem ser alteradas após a criação
+
+print("***Operadores de tuplas***")
+
+#fatiamento de tuplas
+tupla5 = (1, 2, 3, 4, 5)
+print(tupla5[1:3]) #saída: (2, 3)
+print(tupla5[:-2]) #saída: (1, 2, 3)
+
+#desempacotamento de tuplas
+tupla6 = (1, 2, 3)
+a, b, c = tupla6
+print(a) #saída: 1
+print(b) #saída: 2
+print(c) #saída: 3
+
+#concatenação de tuplas
+tupla7 = (1, 2, 3)
+tupla8 = (4, 5, 6)
+tupla_concatenada = tupla7 + tupla8
+print(tupla_concatenada) #saída: (1, 2, 3, 4, 5, 6)
+
+#funções nativas
+tupla9 = (1, 2, 3, 4, 5)
+print(len(tupla9)) #saída: 5
+#len retorna o número de elementos na tupla
+
+print(min(tupla9)) #saída: 1
+#min retorna o menor valor da tupla
+
+print(max(tupla9)) #saída: 5
+#max retorna o maior valor da tupla
+
+print(sum(tupla9)) #saída: 15
+#sum retorna a soma dos elementos da tupla
+
+print("***Operadores Matemáticos***")
+
+
+
 
 
